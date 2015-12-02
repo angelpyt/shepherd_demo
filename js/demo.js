ea83b89019f52c3b5d34d39bdb9633ca20d0b05f
Katrina Ezis <katrinaezis@gmail.com>
2015-12-01 21:20:16 -0800

    add em

---
+++ js/demo.js
@@ -0,0 +1,45 @@
+// Start by initializing a new shepherd tour
+
+
+	// Set it's defaults to include the class of 'shepherd-theme-arrows'
+
+
+
+
+
+// Next let's create our first step 
+// Create a new step and call it myStep
+
+	// give it a title, text, and attach it to our example div
+
+
+
+
+// Let's start our tutorial!
+
+
+
+
+
+// Now let's add some more to our first step
+// add two buttons, one to progress to the next
+// step and one to exit the tour
+
+	// let's give our buttons some classes
+	// make one button a primary button
+	// and one button a secondary button
+
+
+
+
+// Add another step to our tutorial and attach it to
+// the div you created
+
+
+
+
+
+
+
+
+
